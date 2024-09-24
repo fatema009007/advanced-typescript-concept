2d1cc5dbf2c7a518de199f043d009abd4f364670
CHALDAL\fatema <fatema@chaldal.net>
2024-09-25 00:34:11 +0600

    Add generic function for printing values

---
+++ 1-working-with-generic/1-1-generics/generic-function.js
@@ -0,0 +1,12 @@
+function printSomething(arg) {
+    console.log(arg);
+    return arg;
+}
+printSomething('Hello World');
+printSomething(10);
+// To run this TypeScript file, you need to first compile it to JavaScript using the TypeScript compiler (tsc).
+// Open your terminal and navigate to the directory containing the TypeScript file, then run:
+// tsc generic-function.ts
+// This will generate a JavaScript file named `generic-function.js` in the same directory.
+// You can then run the generated JavaScript file using Node.js:
+// node generic-function.js
